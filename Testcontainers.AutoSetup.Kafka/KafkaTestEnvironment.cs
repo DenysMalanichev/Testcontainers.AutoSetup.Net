d21772709cc39bc93527a73cd4dbc985bc4cb06e
Denys Malanichev <denisKirillov228@gmail.com>
2026-03-19 17:29:25 +1100

    set containers' start order

--- Testcontainers.AutoSetup.Kafka/KafkaTestEnvironment.cs
+++ Testcontainers.AutoSetup.Kafka/KafkaTestEnvironment.cs
@@ -38,14 +38,12 @@ public class KafkaTestEnvironment
         if(KafkaNetwork is not null)
             await KafkaNetwork.CreateAsync(cancellationToken);
 
-        List<Task> containersToStart = [KafkaContainer.StartAsync(cancellationToken)];
+        await KafkaContainer.StartAsync(cancellationToken);
         if(KafkaUiContainer is not null)
-            containersToStart.Add(KafkaUiContainer.StartAsync(cancellationToken));
+            await KafkaUiContainer.StartAsync(cancellationToken);
 
         if(SchemaRegistryContainer is not null)
-            containersToStart.Add(SchemaRegistryContainer.StartAsync(cancellationToken));
-
-        await Task.WhenAll(containersToStart);
+            await SchemaRegistryContainer.StartAsync(cancellationToken);
     }
 
     /// <summary>
